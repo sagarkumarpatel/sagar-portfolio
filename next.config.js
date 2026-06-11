eca56064bf83e072d3fb72827d7625ef6aa56920
sagarkumarpatel <sagarpatel98212@gmail.com>
2026-06-11 19:33:05 +0530

    Fix: Remove invalid turbopack config and add critters

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
   images: {
     domains: ['github.com', 'avatars.githubusercontent.com'],
     formats: ['image/avif', 'image/webp'],
